e1531b21d6a95168ecc47a4dc9e9a1446841cfaa
Santiago Fraire Willemoës <santiwilly@gmail.com>
2019-11-03 13:50:53 +0100

    feat: new line highlight

--- scripts/build-with-contrast.sh
+++ scripts/build-with-contrast.sh
@@ -1,9 +1,9 @@
 npm run build
 
 sed -i -e 's#css/theme/night.css#css/theme/beige.css#g' src/index.html
-sed -i -e 's#lib/css/monokai.css#lib/css/zenburn.css#g' src/index.html
+sed -i -e 's#build/styles/agate.min.css#build/styles/zenburn.min.css#g' src/index.html
 
 npm run build:contrast
 
 sed -i -e 's#css/theme/beige.css#css/theme/night.css#g' src/index.html
-sed -i -e 's#lib/css/zenburn.css#lib/css/monokai.css#g' src/index.html
+sed -i -e 's#build/styles/zenburn.min.css#build/styles/agate.min.css#g' src/index.html
